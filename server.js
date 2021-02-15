a98306d9b8cb29b2d91b90acb021d17796604257
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-02-15 11:05:41 +0700

    development

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
